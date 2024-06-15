38c9461490a73b2765c712810ebad84ddee4df38
Daniil <forfaceitsemik@gmail.com>
2024-06-15 11:37:08 +0300

    added id to types

--- types/types.go
+++ types/types.go
@@ -1,11 +1,13 @@
 package types
 
 type Person struct {
+	Id       int    `json:"id"`
 	Name     string `json:"name"`
 	LastName string `json:"last_name"`
 }
 
 type Todo struct {
+	Id     int    `json:"id"`
 	Title  string `json:"title"`
 	Status int    `json:"status"`
 	Author Person `json:"author"`
